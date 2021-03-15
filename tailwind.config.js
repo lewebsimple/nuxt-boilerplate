fcc11d5d5106a2c1fd3e24e5b29f3278e49565f6
Pascal Martineau <pascal@lewebsimple.ca>
2021-03-15 19:59:51 -0400

    Update @nuxtjs/tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 export default {
+  darkMode: false,
+  plugins: [],
+  purge: [],
   theme: {
     container: {
       center: true,
       padding: "1rem",
     },
+    extend: {},
+  },
+  variants: {
+    extend: {},
   },
 };
