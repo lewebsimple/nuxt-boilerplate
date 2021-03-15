03807c542cce1bbd6d2ddc94522165fa728b9944
Pascal Martineau <pascal@lewebsimple.ca>
2021-03-15 16:53:05 -0400

    Replace PWA with simpler head config

---
+++ config/head.js
@@ -0,0 +1,10 @@
+const description = "Nuxt.js boilerplate description";
+
+export default {
+  titleTemplate: "%s - Nuxt.js boilerplate",
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { hid: "description", name: "description", content: description },
+  ],
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,10 @@
 import buildModules from "./config/build-modules";
+import head from "./config/head";
 
 export default {
   buildModules,
   components: true,
+  head,
   css: ["~/assets/styles/main.scss"],
   srcDir: "./src",
 };
