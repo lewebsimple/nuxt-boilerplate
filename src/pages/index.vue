253c823648730260812c6be499022b30f3851b8b
Pascal Martineau <pascal@lewebsimple.ca>
2021-03-15 17:00:26 -0400

    Remove extraneous packages

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="home">
-    <h1>Nuxt.js</h1>
+    <h1>Nuxt.js boilerplate</h1>
     <Hello />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  head: {
+    title: "Home",
+  },
+});
+</script>
